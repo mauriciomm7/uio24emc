8ae3202c54dafc46bfb81c3c506984d11af26b1d
Mauricio M. M. <MauricioMM7@outlook.com>
2024-07-12 22:49:20 +0200

    created nbs

--- paths.py
+++ paths.py
@@ -1,15 +1,17 @@
 # Automatically generated file for accessing correct folders. 
-
 import os
-
 rep_dir = os.path.abspath(os.path.dirname(__file__))
 
-data_dir = os.path.join(rep_dir, 'data')
-results_dir = os.path.join(rep_dir, 'results')
-raw_dir = os.path.join(rep_dir, 'raw')
-figures_dir = os.path.join(rep_dir, 'figures')
-scripts_dir = os.path.join(rep_dir, 'scripts')
-nbs_dir = os.path.join(rep_dir, 'nbs')
+data_dir = os.path.join(rep_dir, 'data\\')
+results_dir = os.path.join(rep_dir, 'results\\')
+raw_dir = os.path.join(rep_dir, 'raw\\')
+figures_dir = os.path.join(rep_dir, 'figures\\')
+scripts_dir = os.path.join(rep_dir, 'scripts\\')
+nbs_dir = os.path.join(rep_dir, 'nbs\\')
 #####################################
-
- # Add your Latex paths here
+# Add your Latex paths here
+# - [ ] Script to generate paths
+tex_base = "C:\\Users\\mauricmm\\UiO Dropbox\\Mauricio Mandujano Manriquez\\Apps\\Overleaf\\DRAFT_uio24emc_referral_failure_mmmanriquez\\"
+tex_bib_file = os.path.join(tex_base, "references.bib")
+tex_fig_dir = os.path.join(tex_base, "figures")
+tex_tab_dir = os.path.join(tex_base, "tables")
